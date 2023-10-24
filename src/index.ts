dc7e5d81759a5fd0776b3faff9bad36168ccc81f
hirenf14 <hirenf14@gmail.com>
2023-10-24 19:02:01 +0530

    feat: added authProvider hook

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export const AuthProvider = {};
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+
+export { useAuthProvider, AuthProviderHookResult } from "./hooks/useAuthProvider";
+
+export const RefineKindeProvider = KindeProvider;
+export default RefineKindeProvider;
